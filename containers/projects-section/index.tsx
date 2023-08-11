93c3d3400a0b21ddae56961a95bbe03ce3afded3
Merto <mertenercan@gmail.com>
2023-08-11 07:54:32 +0300

    - added api folder

--- containers/projects-section/index.tsx
+++ containers/projects-section/index.tsx
@@ -4,14 +4,14 @@ import axios from 'axios'
 interface ProjectsSectionProps {
   
 }
-const getProjects = async() => {
+/*const getProjects = async() => {
   const endpoint = process.env.URL_ROOT ?? "http://localhost:3000" ;
   const data = await axios(endpoint+'/api/projects')
   return data.data
-}
+}*/
 const ProjectsSection: FC<ProjectsSectionProps> = async({  }) => {
-  const data = await getProjects()
-  console.log(data.projects[0].name)
+  /*const data = await getProjects()
+  console.log(data.projects[0].name)*/
   return (
     <section className='w-full flex justify-center'>
      <div className="w-full max-w-screen-lg flex flex-col gap-5">
@@ -20,7 +20,7 @@ const ProjectsSection: FC<ProjectsSectionProps> = async({  }) => {
         </div>
         <div className="-mx-4">
             <Card></Card>
-            {<h1 className='text-white'>{data.projects[0].name}</h1>}
+            {/*<h1 className='text-white'>{data.projects[0].name}</h1>*/}
         </div>
      </div>
     </section>
