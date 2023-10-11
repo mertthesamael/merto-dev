93c9c3fba7325fa079aff5c680568863e3760edc
Merto <mertenercan@gmail.com>
2023-10-11 08:07:20 +0300

    - SEO optimizations

---
+++ app/[locale]/robots.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from 'next'
+
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : 'http://localhost:3000';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*'
+      }
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl
+  };
+}
