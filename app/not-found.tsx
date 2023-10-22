09afa25671da13770b2361ac7a0dc2931dfe4b5e
Merto <mertenercan@gmail.com>
2023-10-22 14:02:46 +0300

    - minor refactor

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { söhne } from '@/libs/fonts';
-import { ThemeProvider } from './theme-provider';
 import Link from 'next/link';
 import '@/styles/global.css'
 // Render the default Next.js 404 page when a route
@@ -11,11 +10,9 @@ export default function NotFound() {
     return (
         <html lang="en" className={söhne.className}>
             <title>Not Found :/</title>
-            <ThemeProvider defaultTheme='dark'>
-                <body className='w-screen h-screen grid place-items-center'>
+                <body className='w-screen h-screen grid place-items-center bg-black'>
                     <Link href={'/en'} className='transition-all font-bold text-[6rem] hover:text-[#00FF7F]'>4 0 4</Link>
                 </body>
-            </ThemeProvider>
         </html>
     );
 }
