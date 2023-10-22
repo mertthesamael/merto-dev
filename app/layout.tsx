2c746939906f1b46d3b6e47c6679cdfa97f21cff
Merto <mertenercan@gmail.com>
2023-10-22 13:52:09 +0300

    - not found page template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import {ReactNode} from 'react';
+import { poppins, söhne, urbanist } from '@/libs/fonts';
+import { ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
@@ -6,6 +7,6 @@ type Props = {
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
-export default function RootLayout({children}: Props) {
-  return children;
+export default function RootLayout({ children }: Props) {
+  return children
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,21 @@
 'use client';
-
-import Error from 'next/error';
-
+import { söhne } from '@/libs/fonts';
+import { ThemeProvider } from './theme-provider';
+import Link from 'next/link';
+import '@/styles/global.css'
 // Render the default Next.js 404 page when a route
 // is requested that doesn't match the middleware and
 // therefore doesn't have a locale associated with it.
 
 export default function NotFound() {
-  return (
-    <html lang="en">
-      <body>
-        <Error statusCode={404} />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className={söhne.className}>
+            <title>Not Found :/</title>
+            <ThemeProvider defaultTheme='dark'>
+                <body className='w-screen h-screen grid place-items-center'>
+                    <Link href={'/en'} className='transition-all font-bold text-[6rem] hover:text-[#00FF7F]'>4 0 4</Link>
+                </body>
+            </ThemeProvider>
+        </html>
+    );
 }
