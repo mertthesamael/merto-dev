a29ad36155c7b88cbb9bec3427fe7527f2dda6c6
Merto <mertenercan@gmail.com>
2023-11-14 07:16:08 +0300

    - partial rendering implementatins w/ fallback

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,17 +6,6 @@ import axios from "axios";
 import { getTranslator } from "next-intl/server";
 import { Suspense } from "react";
 
-const getProjects = async (filter: string) => {
-  const endpoint = process.env.URL_ROOT ?? "http://localhost:3000";
-  try {
-    const data = await axios.post(endpoint + "/api/projects", {
-      sort: filter ? filter : "all",
-    });
-    return data.data.projects;
-  } catch (err) {
-    return err;
-  }
-};
 
 export default async function Home({
   params: { locale },
@@ -30,18 +19,14 @@ export default async function Home({
   const filter =
     typeof searchParams.filter === "string" ? searchParams.filter : "all";
 
-  const projects: ProjectType[] = await getProjects(filter);
 
   return (
     <main className="flex my-10 md:my-20 flex-col items-center gap-[--header-height] pt-[--header-height]">
       <InfoSection welcome={t("welcome")} info={t.raw("info")} />
-      <Suspense key={filter} fallback={<h1 className="text-white font-urbanist font-bold text-2xl">Loading Projects..</h1>}>
         <ProjectsSection
           title={t("projects")}
-          filter={searchParams.filter}
-          projects={projects}
+          filter={filter}
         />
-      </Suspense>
       <DonateSection text={t("donate")} />
     </main>
   );
--- containers/projects-section/index.tsx
+++ containers/projects-section/index.tsx
@@ -1,39 +1,68 @@
 "use client"
 import Card from "@/components/Card";
-import React, { FC } from "react";
+import React, { FC, Suspense } from "react";
 import { ProjectType } from "@/types/ProjectType";
 import MultiButtonFilter from "@/components/MultiButtonFilter";
 import { motion } from "framer-motion";
+import axios from "axios";
+
 
 
 interface ProjectsSectionProps {
   title: string;
-  filter:string,
-  projects:ProjectType[]
+  filter: string,
 }
 
-const ProjectsSection: FC<ProjectsSectionProps> =  ({ title, projects }) => {
-  const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        delayChildren: 0.2,
-        staggerChildren: 0.2
-      }
+const getProjects = async (filter: string) => {
+  const endpoint = process.env.URL_ROOT ?? "http://localhost:3000";
+  try {
+    const data = await axios.post(endpoint + "/api/projects", {
+      sort: filter ? filter : "all",
+    });
+    return data.data.projects;
+  } catch (err) {
+    return err;
+  }
+};
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.2
     }
-  };
-  
-  const item = {
-    hidden: { x: 40, opacity: 0 },
-    visible: {
-      x: 0,
-      opacity: 1
-    }
-  };
+  }
+};
 
+const item = {
+  hidden: { x: 40, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1
+  }
+};
+const ProjectsContainer = async ({ filter }: { filter: string }) => {
+  
+  const projects: ProjectType[] = await getProjects(filter);
   const projectsArray = [...projects].reverse()
+
+
+  return (
+    <>
+      {projectsArray.map((project, _i) => (
+        <motion.div key={project.name} variants={item} className="w-full">
+          <Card project={project} />
+        </motion.div>
+      ))}
+    </>
+  )
+}
+const ProjectsSection: FC<ProjectsSectionProps> = ({ title, filter }) => {
+
+
+
   return (
     <section className="w-full flex justify-center">
       <div className="w-full max-w-screen-lg flex flex-col gap-5 mx-8">
@@ -46,19 +75,18 @@ const ProjectsSection: FC<ProjectsSectionProps> =  ({ title, projects }) => {
           <MultiButtonFilter />
         </div>
         <motion.div
-        variants={container}
-        initial="hidden"
-        animate="visible"
+          variants={container}
+          initial="hidden"
+          animate="visible"
           style={{
             gridTemplateColumns: "repeat(auto-fill, minmax(340px, 1fr))",
           }}
-          className="-mx-4 flex lg:grid flex-wrap justify-center md:gap-[1.12rem] gap-4 md:justify-between"
-        >
-          {projectsArray.map((project,_i) => (
-            <motion.div key={project.name}  variants={item} className="w-full">
-              <Card project={project} />
-            </motion.div>
-          ))}
+          className="-mx-4 flex lg:grid flex-wrap justify-center md:gap-[1.12rem] gap-4 md:justify-between">
+          <Suspense key={filter} fallback={Array(6).fill(0).map((el, _i) => (
+            <motion.div variants={item} className="animate-pulse w-full md:w-80 h-[22rem]  rounded-lg bg-black/10 dark:bg-white/10" />
+          ))}>
+            <ProjectsContainer filter={filter} />
+          </Suspense>
         </motion.div>
       </div>
     </section>
