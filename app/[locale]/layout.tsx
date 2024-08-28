d561b64e2682e9204389ddadca78a378f53b9ed9
Mert Enercan <mertenercan@gmail.com>
2024-08-28 16:31:54 +0300

    - pixel testing

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -45,23 +45,25 @@ export default function LocaleLayout({
           strategy="afterInteractive"
         >
           {`
-         !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '858233815916794');
-  fbq('track', 'PageView');
+        !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1027652965507750');
+fbq('track', 'PageView');
         `}
         </Script>
-        <noscript><img height="1" width="1" style={{"display":"none"}}
-          src="https://www.facebook.com/tr?id=858233815916794&ev=PageView&noscript=1"
+        <noscript><img height="1" width="1" style={{ "display": "none" }}
+          src="https://www.facebook.com/tr?id=1027652965507750&ev=PageView&noscript=1"
         /></noscript>
       </body>
       <Analytics />
     </html>
   )
 }
+
+
